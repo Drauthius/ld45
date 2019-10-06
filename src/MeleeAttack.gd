9e696ad6698da5f6eb3ba16f139e2073f96367ea
Drauthius <albert@diserholt.com>
2019-10-06 12:54:25 +0200

    Add enemy placeholder

--- src/MeleeAttack.gd
+++ src/MeleeAttack.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
+export(int, 1, 100) var damage := 1
+
 func _ready():
 	$AnimationPlayer.play("default")
 
 func _on_AnimationPlayer_animation_finished(_anim_name):
 	queue_free()
+
+func _on_MeleeAttack_body_entered(body):
+	if body != $".." and body.is_in_group("Living"):
+		body.take_damage(damage)
