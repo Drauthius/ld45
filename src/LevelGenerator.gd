ac4ed2d76359eac40f1a50c6632482752cb67b3e
Drauthius <albert@diserholt.com>
2019-10-07 11:56:42 +0200

    Improve level generator
    Creates a corridor of sorts for now.

--- src/LevelGenerator.gd
+++ src/LevelGenerator.gd
@@ -1,55 +1,100 @@
 extends Node
 class_name LevelGenerator
 
-const TileDefinition = "res://dat/tiles2.json"
-#const RoomDefinition = "res://dat/rooms.json"
+const TileDefinition = "res://dat/tiles.json"
 
 const Chest = preload("res://scn/Chest.tscn")
 
-const Size = { w = 16, h = 12 }
+const Size = { w = 12, h = 8 }
 
 enum Direction { LEFT, RIGHT, UP, DOWN }
 
 var tilemap : TileMap
 var tileDefs
+var map := {}
+var path := []
 
 func fill(node, _tilemap, _level):
 	tilemap = _tilemap
 	tileDefs = _read_json(TileDefinition)
-	
-	var map = {}
-	var path = []
-	
-	var dir = randi() % Direction.size()
-	var x = 0
-	var y = 0
-	_create_terminal_room(dir, x, y)
-	match dir:
-		Direction.LEFT: x = x - Size.w
-		Direction.RIGHT: x = x + Size.w
-		Direction.UP: y = y - Size.h
-		Direction.DOWN: y = y + Size.h
-	_create_intersection(x, y)
 
-func _create_terminal_room(dir : int, x : int, y : int) -> void:
-	if dir != Direction.UP:
+	var dir = randi() % Direction.size()
+	var room := Vector2(0, 0)
+	map[room.x] = {}
+	map[room.x][room.y] = dir
+	
+	# warning-ignore:return_value_discarded
+	_create_room(dir, 0, room)
+	path.append(room)
+	
+	while true:
+		room = _translate(room, dir)
+		dir = _flip(dir)
+		
+		var num_exits = 1 if path.size() < 4 else 0
+		var open = _create_room(dir, num_exits, room)
+		path.append(room)
+		
+		if open.size() == 0:
+			break
+		
+		dir = open[randi() % open.size()]
+
+func _translate(room : Vector2, dir : int) -> Vector2:
+	match dir:
+		Direction.LEFT: return room + Vector2(-1, 0)
+		Direction.RIGHT: return room + Vector2(1, 0)
+		Direction.UP: return room + Vector2(0, -1)
+		Direction.DOWN: return room + Vector2(0, 1)
+
+func _flip(dir : int) -> int:
+	match dir:
+		Direction.LEFT: return Direction.RIGHT
+		Direction.RIGHT: return Direction.LEFT
+		Direction.UP: return Direction.DOWN
+		Direction.DOWN: return Direction.UP
+
+# warning-ignore:unused_argument
+func _create_room(from : int, num_open : int, room : Vector2) -> Array:
+	var available = []
+	for dir in Direction.values():
+		var r = _translate(room, dir)
+		if not map.has(r.x) or not map[r.x].has(r.y):
+			available.append(dir)
+	
+	var open = {}
+	for dir in range(Direction.size()):
+		open[dir] = from == dir
+	
+	# warning-ignore:narrowing_conversion
+	num_open = min(available.size() - 1, num_open)
+	while num_open > 0:
+		var dir = randi() % Direction.size()
+		if not open[dir]:
+			open[dir] = true
+			num_open -= 1
+	
+	var x : int = room.x * Size.w
+	var y : int = room.y * Size.h
+	
+	if not open[Direction.UP]:
 		for ox in range(x, x + Size.w):
 			_set_cell(ox, y, tileDefs["bottom-wall"])
 			_set_cell(ox, y+1, tileDefs["top-wall"])
 	
-	if dir != Direction.LEFT:
+	if not open[Direction.LEFT]:
 		for oy in range(y, y + Size.h):
 			_set_cell(x, oy, tileDefs["side-wall"])
 		if _get_cell(x + 1, y + 1) != -1:
 			_set_cell(x + 1, y + 1, tileDefs["topleft-wall"])
 	
-	if dir != Direction.RIGHT:
+	if not open[Direction.RIGHT]:
 		for oy in range(y, y + Size.h):
 			_set_cell(x + Size.w - 1, oy, tileDefs["side-wall"])
 		if _get_cell(x + Size.w - 2, y + 1) != -1:
 			_set_cell(x + Size.w - 2, y + 1, tileDefs["topright-wall"])
 	
-	if dir != Direction.DOWN:
+	if not open[Direction.DOWN]:
 		for ox in range(x, x + Size.w):
 			_set_cell(ox, y + Size.h - 1, tileDefs["bottom-wall"])
 	
@@ -57,9 +102,12 @@ func _create_terminal_room(dir : int, x : int, y : int) -> void:
 		for oy in range(y, y + Size.h):
 			if _get_cell(ox, oy) == -1:
 				_set_cell(ox, oy, tileDefs["ground"])
-
-func _create_intersection(x : int, y : int) -> void:
-	pass
+	
+	var dirs = []
+	for dir in open.keys():
+		if open[dir] and dir != from:
+			dirs.append(dir)
+	return dirs
 
 func _get_cell(x, y):
 	return tilemap.get_cell(x, y) #y, x)
@@ -67,8 +115,8 @@ func _get_cell(x, y):
 func _set_cell(x, y, tileDef):
 	tilemap.set_cell(x, y, #y, x,
 		tileDef["tiles"][randi() % tileDef["tiles"].size()],
-		true if tileDef.has("flip_x") and tileDef["flip_x"] and randf() < 0.5 else false,
-		true if tileDef.has("flip_y") and tileDef["flip_y"] and randf() < 0.5 else false)
+		tileDef.has("flip_x") and tileDef["flip_x"] and randf() < 0.5,
+		tileDef.has("flip_y") and tileDef["flip_y"] and randf() < 0.5)
 
 #func fill(node, tilemap, _level):
 #	var tileDef = read_json(TileDefinition)
